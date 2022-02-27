6d8261d78c2b6eaad1a416123ad84f669a307adc
Alexander <alexander.c1989@gmail.com>
2022-02-27 00:46:01 -0500

    adding 404 page view

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get ('/', (req, res) => {
 const breadsController = require('./controllers')
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>');
+    res.status(404).render('<h1>404 Page</h1>');
 })
 
 app.get ('/second', function(req, res) {
