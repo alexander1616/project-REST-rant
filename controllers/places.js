af70b32e65e2dd02194869587602edc3d6d01f4c
Alexander <alexander.c1989@gmail.com>
2022-02-27 01:41:09 -0500

    adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -4,4 +4,14 @@ router.get('/', (req, res) => {
     res.send('GET /places')
 })
 
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    res.render('idetail', { 
+    })
+})
+
 module.exports = router
